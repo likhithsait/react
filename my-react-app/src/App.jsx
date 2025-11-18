6b7a514fcde27b3ef1131fe096e9241ae87f86e7
likhithsait <likhithsai.t@narayanagroup.com>
2025-11-18 12:29:15 +0530

    Components basics

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,32 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+function Wlecome(){
+return(
+  <p>
+    Welcome!!
+  </p>
+);
+}
+function Button(){
+  return <button >Click me</button>;
+}
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>
+            react tutorial
+        </h1>
+         <Wlecome/>
+        <Button/>
       </div>
-      <h1>Hii</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          hello world
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
